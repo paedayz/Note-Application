aa541e1877912eab4b05ee5933c27f6fc94b7cb1
paedayz <pae16phasit@gmail.com>
2020-08-27 23:19:09 +0700

    [Added]: useSelector of notes state

--- react-redux-typescript/src/App.tsx
+++ react-redux-typescript/src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
 
+// Redux
+import { useSelector } from "react-redux";
+import { NotesState } from "./noteReducer";
+
 // Component
 import NewNoteInput from "./NewNoteInput";
 
 function App() {
+  const notes = useSelector<NotesState, NotesState["notes"]>(
+    (state) => state.notes
+  );
   return (
     <div>
       <NewNoteInput addNote={alert} />
